ba0388065664f424c727fd8d2d7360bd1a59c376
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-10-31 16:53:47 -0700

    task100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('z'), ord('a') - 1, -1):
-    print(f"{chr(letter)}{chr(letter - 32) if letter % 2 == 0 else ''}", end='')
+    if letter % 2 == 0:
+        print(f"{chr(letter)}{chr(letter - 32)}", end='')
