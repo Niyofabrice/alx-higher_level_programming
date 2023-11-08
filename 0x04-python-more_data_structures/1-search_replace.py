f347f703bbc6945ba83e9e842a90215f79cba461
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-11-08 11:03:20 -0800

    task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
     for number in range(len(my_list)):
         if my_list[number] == search:
-            my_list[number] = replace
-    return my_list
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[number])
+    return new_list
