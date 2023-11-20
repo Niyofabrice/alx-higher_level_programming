dd0f54af86c3f578c3f77d3a277402031cfbe24e
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-11-20 06:38:33 -0800

    debugged task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     n_elements = 0
     for element in range(x):
         try:
-            print(my_list[element])
+            print("{}".format(my_list[element]), end="")
             n_elements += 1
-            print()
         except IndexError:
+            break
     print()
     return (n_elements)
