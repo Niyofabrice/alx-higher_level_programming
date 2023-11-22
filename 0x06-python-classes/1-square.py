70df98a39f0a307db41f621acad5f158de31249c
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-11-22 15:54:23 -0800

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Square class definition'''
+
+
+class Square:
+    '''Represents a square
+
+    Attributes:
+        __size (int): size of side of square
+    '''
+    def __init__(self, size):
+        '''Initializes a square
+
+        Args:
+            size (int): size of side of square
+
+        Returns: None
+        '''
+        self.__size = size
