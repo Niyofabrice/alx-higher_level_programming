0ad5ed71e83c23b4cfd0ab4342cb6e1e5d25404b
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-11-27 12:43:51 -0800

    task1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines class Rectangle"""
+
+class Rectangle:
+    """class that defines properties of a rectangle"""
+    def __init__(self, width=0, height=0):
+        """creating new instance of Rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieving the width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Retrieving the height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """setting the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """setting the height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        
+        self.__height = value
