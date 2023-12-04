573a03e39cf23a41e5ca2fa9340231426053011d
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-04 10:15:11 -0800

    task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" Contains the lookup function """
+"""Returns a function lookup()"""
+
 
 def lookup(obj):
-    """ function that returns list of available attributes
-    and methods of an object
+    """Function that returns the list of available attributes and methods,
+    f an object
 
-   Args:
+    Args:
         obj (class): object
 
-   Returns:
+    Returns:
         list: list of available attributes and methods of an object
     """
-
     return dir(obj)
