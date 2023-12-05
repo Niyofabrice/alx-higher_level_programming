9dbc1f879bf5aee988b3a734516d78a72842d745
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-05 12:10:17 -0800

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-""" Contains function that reads a text file """
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """ Function that reads a text file
-    Args:
-    filename: the name of the file to read
-    """
-    with open(filename, 'r', encoding="utf-8") as f:
-        data = f.read()
-        print(data)
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
