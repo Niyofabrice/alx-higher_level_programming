d114a7b729a0717070e347a7abfe9857cf82a6c7
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-11 10:14:24 -0800

    added a method that returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,10 +17,9 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ List to JSON string """
-        if list_dictionaries is None or list_dictionaries == "[]":
-            return "[]"
-       else:
-           return json.dumps(list_dictionaries)
+        if list_dictionaries is not None:
+            return json.dumps(list_dictionaries)
+        return "[]"
 
     @classmethod
     def save_to_file(cls, list_objs):
