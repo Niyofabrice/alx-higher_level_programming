5982bfc62dfe5491e6b1509d922074d4c0d2b6b6
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-11 09:40:03 -0800

    added toddictionary function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" defines class Rectangle """
+""" Module that contains class Rectangle, inheritance of class Base """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ class that inherits from Base """
+    """ Class Rectangle """
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ constructor function """
-        supper().__init__(id)
+        """ Initializes instances """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
@@ -39,11 +40,11 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-        self.__width = value
+        self.__height = value
 
     @property
     def x(self):
-        """ x gettter """
+        """ x getter """
         return self.__x
 
     @x.setter
@@ -52,7 +53,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be >=0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -66,26 +67,47 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be >=0")
+            raise ValueError("y must be >= 0")
         self.__y = value
-    
+
     def area(self):
-        """ function that returns the area of a rectangle """
-        return (self.width * self.height)
+        """ returns the area of the rectangle object """
+        return self.width * self.height
 
     def display(self):
-        """ function that prints in stdout the Rectangle instance with # """
+        """ displays a rectangle """
         rectangle = self.y * "\n"
         for i in range(self.height):
             rectangle += (" " * self.x)
             rectangle += ("#" * self.width) + "\n"
 
-        print(rectangle, end="")
+        print(rectangle, end='')
 
     def __str__(self):
-        """ modified str function """
-        str_rectangle = "[Rectangle]"
-        str_id = "({})".format(self.id)
-        str_xy = "{}/{} - ".format(self.__x, self.__y)
-        str_wh = "{}/{}".format(self.__width, self.__height)
+        """ str special method """
+        str_rectangle = "[Rectangle] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
         return str_rectangle + str_id + str_xy + str_wh
+
+    def update(self, *args, **kwargs):
+        """ update method """
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args)):
+                setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ method that returs a dictionary with properties """
+        list_atr = ['id', 'width', 'height', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            dict_res[key] = getattr(self, key)
+
+        return dict_res
