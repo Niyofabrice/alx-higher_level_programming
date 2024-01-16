6c94a205ff2a767a66a2327936f0c290c6751559
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-01-16 04:33:31 -0800

    changes properties of a database and of a table

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- Changes a property of a database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Changes the property of a table
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
