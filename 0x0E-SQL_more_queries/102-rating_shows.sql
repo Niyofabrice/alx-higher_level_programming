6cc6d3f988019c859679d2f648cf1ac624d35c3c
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-01-17 12:07:34 -0800

    Lists all shows without the genre Comedy in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_ratings
+     ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
