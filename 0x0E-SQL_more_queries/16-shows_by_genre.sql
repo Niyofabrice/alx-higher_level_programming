ef36d2982d6a8d0075be61ff5cb0f200b3776076
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-01-17 12:01:46 -0800

    lists all shows and genres linked to the show from the hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,10 @@
 -- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT s.title, g.name
-FROM tv_shows AS s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+SELECT
+tv_shows.title AS title,
+tv_genres.name AS name
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title, name;
