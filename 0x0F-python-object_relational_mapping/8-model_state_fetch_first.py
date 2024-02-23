913a3f6dc7dbb7f1a624189727cf0ddbabfc534b
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-02-23 12:41:15 -0800

    task8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
 """
-    A script that returns the first State object from hbtn_0e_6_usa
-    Username, password and dbname wil be passed as arguments to the script.
+Script that prints the first State object from the database
 """
 
-
-import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+if __name__ == "__main__":
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
     Base.metadata.create_all(engine)
+    s_tate = session.query(State).order_by(State.id).first()
 
-    # create a session
-    session = Session()
-
-    # extract first state
-    states = session.query(State).order_by(State.id).first()
-
-    # print state
-    if states is None:
-        print("Nothing")
+    if s_tate:
+        print("{}: {}".format(s_tate.id, s_tate.name))
     else:
-        print("{}: {}".format(states.id, states.name))
-
+        print("Nothing")
     session.close()
