0bd0e55727834e036daa13cd804a944dc6244d58
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-02-23 12:46:55 -0800

    task5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
-
 """
-    A script that lists all cities in a state from the database hbtn_0e_0_usa
-    Username, password and database name and state are given as user args
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database
 """
-
-
-import sys
 import MySQLdb
+from sys import argv
 
-
+# The code should not be executed when imported
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    cursor = db.cursor()
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s", [argv[4]])
 
-    sql = """SELECT cities.name
-          FROM states
-          INNER JOIN cities ON states.id = cities.state_id
-          WHERE states.name = %s
-          ORDER BY cities.id ASC"""
+    rows = cur.fetchall()
+    j = []
+    for i in rows:
+        j.append(i[1])
+    print(", ".join(j))
 
-    cursor.execute(sql, (sys.argv[4],))
-
-    data = cursor.fetchall()
-
-    print(", ".join([city[0] for city in data]))
-
-    cursor.close()
+    # Clean up process
+    cur.close()
     db.close()
