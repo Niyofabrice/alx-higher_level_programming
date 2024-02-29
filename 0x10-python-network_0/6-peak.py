654b42f5c157318f955085cc5c5d1ca5c2840fe8
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-02-29 11:11:50 -0800

    task6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Contains a function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    start = 0
+    end = len(list_of_integers)
+    mid = ((end - start) // 2) + start
+    mid = int(mid)
+    if end == 1:
+        return list_of_integers[0]
+    if end == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
