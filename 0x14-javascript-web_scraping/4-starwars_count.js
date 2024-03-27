f6db956333586f867fd21a8cdffe7f0155879c9e
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-03-27 12:11:00 -0700

    Prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,8 +5,14 @@ const request = require('request');
 const endPoint = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(body && JSON.parse(body).title);
+    console.log(err)
+  } else if (body) {
+    // Wedge Antilles is character ID 18 - use this ID for filtering the
+    // result of the API
+    const json = JSON.parse(body);
+    const charFilms = json.results.filter(
+      x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+    );
+    console.log(charFilms.length);
   }
 });
