f2e8f0cae76d131efc28026bbef6d138210324de
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-03-27 12:13:40 -0700

    Prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,24 @@
 #!/usr/bin/node
-// Prints the number of movies where the character “Wedge Antilles” is present
 
 const request = require('request');
-const endPoint = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
   if (err) {
-    console.log(err)
-  } else if (body) {
-    // Wedge Antilles is character ID 18 - use this ID for filtering the
-    // result of the API
-    const json = JSON.parse(body);
-    const charFilms = json.results.filter(
-      x => x.characters.find(y => y.match(/\/people\/18\/?$/))
-    );
-    console.log(charFilms.length);
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
