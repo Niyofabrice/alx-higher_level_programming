870bc075534a8f1fbe208fa7af0004708b3ffafa
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-03-27 11:34:04 -0700

    Reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Reads and prints the content of a file
 
-const argv = process.argv;
+const filePath = process.argv[2];
 let fs = require('fs');
-fs.readFile(argv[2], 'utf8', function (err, data) {
+fs.readFile(filePath, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
