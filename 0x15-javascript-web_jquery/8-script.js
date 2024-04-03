594c4697d5f342be0fe0e659b1dbb3a2c604099f
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-04-03 13:02:22 -0700

    script that fetches data from api

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+const $ = window.$;
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  console.log(data);
+  data.results.forEach(film => {
+    $('UL#list_movies').append(film.title);
+  });
+});
